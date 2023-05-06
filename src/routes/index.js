191ca2c8f55ad08d5f7342d47bbc35bba313bbec
chamathjayasekara99 <chamathjayasekara5962@gmail.com>
2023-05-06 23:41:32 +0530

    product management done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import authRouter from './auth'
-
+import productRouter from './product.routes'
 const router = Router()
 
 router.use('/auth', authRouter)
-
+router.use('/products', productRouter)
 export default router
