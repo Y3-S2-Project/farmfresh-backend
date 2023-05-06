fc95c90cad960b195ab5e38dc991beeb878e0972
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-07 01:04:33 +0530

    Review create added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRouter from './auth'
 import orderRouter from './order'
+import reviewRouter from './review'
 
 const router = Router()
 
 router.use('/auth', authRouter);
 router.use('/order', orderRouter);
+router.use('/review', reviewRouter)
 
 export default router
