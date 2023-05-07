8d277208828e8a81c48e3547bad1e4de5be473ba
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-07 09:52:31 +0530

    Create payment added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRouter from './auth'
 import orderRouter from './order'
+import paymentRouter from './payment'
 
 const router = Router()
 
 router.use('/auth', authRouter);
 router.use('/order', orderRouter);
+router.use('/payment', paymentRouter)
 
 export default router
