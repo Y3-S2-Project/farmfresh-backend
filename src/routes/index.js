a2669059e6ca83308aacd12076b6f971d3ea9d85
chamathjayasekara99 <chamathjayasekara5962@gmail.com>
2023-05-07 08:33:03 +0530

    category management-create category method added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import authRouter from './auth'
 import orderRouter from './order'
-
+import categoryRouter from './category'
 const router = Router()
 
-router.use('/auth', authRouter);
-router.use('/order', orderRouter);
+router.use('/auth', authRouter)
+router.use('/order', orderRouter)
+router.use('/categories', categoryRouter)
 
 export default router
