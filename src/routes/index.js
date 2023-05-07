99dd1996b548799791773ba663725f514f0fcad2
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-07 09:31:22 +0530

    Create cart added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRouter from './auth'
 import orderRouter from './order'
+import cartRouter from './cart'
 
 const router = Router()
 
 router.use('/auth', authRouter);
 router.use('/order', orderRouter);
+router.use('/cart', cartRouter);
 
 export default router
