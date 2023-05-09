60af5e772c215df3d8890aac0094755f704e3751
IT20636524 <IT20636524@my.sliit.lk>
2023-05-10 00:26:36 +0530

    order backend

--- src/repositories/order.js
+++ src/repositories/order.js
@@ -9,15 +9,15 @@ export const createOrderRepository = async (data) => {
 }
 
 //get all orders from the database
-// export default getAllOrdersRepository = async (order_id) => {
-//     let orders
-//     if (order_id) {
-//       orders = await Order.find({ order_id }).populate.populate('products.product_id')
-//     } else {
-//       orders = await Order.find().populate('products.product_id').populate('user_id')
-//     }
-//     return orders
-// }
+export const getAllOrdersRepository = async (order_id) => {
+    let orders
+    if (order_id) {
+      orders = await Order.find({ order_id }).populate.populate('products.product_id')
+    } else {
+      orders = await Order.find().populate('products.product_id').populate('user_id')
+    }
+    return orders
+}
 
 //get order by order id from the database
 export const getOrderByIdRepository = async (order_id) => {
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { protect } from '../middleware/auth'
 import { ROLES } from '../utils/constants'
-import { createOrder, getDeliveredOrdersCount } from '../controllers/order'
+import { createOrder, getAllOrders, getDeliveredOrdersCount, getOrderById, getPendingOrdersCount, updateOrderStatus, updatePaymentStatus } from '../controllers/order'
 
 const router = Router()
 
-router.route('/').post(protect(ROLES.BUYER), createOrder) //.get(protect(ROLES.BUYER), getAllOrders);
-// router.route('/:id').get(protect(ROLES.BUYER), getOrderById).patch(protect(ROLES.BUYER), updateOrderStatus).delete(protect(ROLES.BUYER), deleteOrder);
+router.route('/').post(protect(ROLES.BUYER), createOrder) .get(protect(ROLES.BUYER), getAllOrders);
+router.route('/:id').get(protect(ROLES.BUYER), getOrderById).patch(protect(ROLES.BUYER), updateOrderStatus).put(protect(ROLES.BUYER), updatePaymentStatus);
+router.route('/getPendingCount').get(protect(ROLES.BUYER), getPendingOrdersCount)
 router.route('/getDeliveryCount').get(protect(ROLES.BUYER), getDeliveredOrdersCount)
 
 export default router
