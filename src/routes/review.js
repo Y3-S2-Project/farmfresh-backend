54b0be29e15902417c41b1cfeca71ba8d54b26d3
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-17 16:07:48 +0530

    Modfied get route

--- src/routes/review.js
+++ src/routes/review.js
@@ -20,8 +20,8 @@ router.put('/:review_id', protect(ROLES.BUYER), updateReviewController)
 //route for deleting a farmer or a product review
 router.delete('/:review_id', protect(ROLES.BUYER), deleteReviewController)
 
-//route for retrieving reviews - specific farmer or specific product
-router.get('/read/', protect(ROLES.BUYER), getFarmerOrProductReviewsController)
+//route for retrieving reviews - specific farmer or specific product (buyer, seller and admin can access this route)
+router.get('/farmer-or-product', getFarmerOrProductReviewsController)
 
 //route to retrieve all reviews
 router.get('/', protect(ROLES.ADMIN), getAllReviewsController)
