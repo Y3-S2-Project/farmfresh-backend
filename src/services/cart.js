26325088d3d2d2c4f3fa5e85e38f6b1ca22e7e6d
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-18 16:40:48 +0530

    update cart, delete a product from cart

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,5 +1,22 @@
-import { createCartRepository } from '../repositories/cart.js'
+import {
+  createCartRepository,
+  updateQuantityRepository,
+  getCartByUserIdRepository,
+  deleteProductFromCartRepository,
+} from '../repositories/cart.js'
 
 export const createCartService = async (data) => {
   return await createCartRepository(data)
+}
+
+export const updateQuantityService = async (user_id, data) => {
+  return await updateQuantityRepository(user_id, data)
+}
+
+export const getCartByUserIdService = async (user_id) => {
+  return await getCartByUserIdRepository(user_id)
+}
+
+export const deleteProductFromCartService = async (user_id, product_id) => {
+  return await deleteProductFromCartRepository(user_id, product_id)
 }
