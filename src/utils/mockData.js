0853500f5b9bd47e194e44097fc5856d37c957f9
IT20636524 <IT20636524@my.sliit.lk>
2023-05-25 22:34:05 +0530

    order test cases

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -56,3 +56,109 @@ export const invalidCategory = {
   caty_status: 'Available',
   cat_id: 'CAT001',
 }
+
+/* order mock data */
+export const orderArray = [
+  {
+    order_id: 'OID001',
+    user_id: '64562c282fb20f06a113bde4',
+    products: [
+      {
+        product_id: '643e85d9f231b52fe5162523',
+        quantity: 3,
+      },
+    ],
+    delivery_id: '645760ea1879fd0016ad3015',
+    totalPrice: 2000,
+    order_status: 'Pending',
+    payment_status: 'Pending',
+  },
+  {
+    order_id: 'OID002',
+    user_id: '64562c282fb20f06a113bde4',
+    products: [
+      {
+        product_id: '643e85d9f231b52fe5162523',
+        quantity: 3,
+      },
+    ],
+    delivery_id: '645760ea1879fd0016ad3015',
+    totalPrice: 2000,
+    order_status: 'Pending',
+    payment_status: 'Pending',
+  },
+  {
+    order_id: 'OID003',
+    user_id: '64562c282fb20f06a113bde4',
+    products: [
+      {
+        product_id: '643e85d9f231b52fe5162523',
+        quantity: 3,
+      },
+    ],
+    delivery_id: '645760ea1879fd0016ad3015',
+    totalPrice: 3000,
+    order_status: 'Pending',
+    payment_status: 'Pending',
+  },
+]
+
+export const order = {
+  order_id: 'OID004',
+  user_id: '64562c282fb20f06a113bde4',
+  products: [
+    {
+      product_id: '643e85d9f231b52fe5162523',
+      quantity: 3,
+    },
+  ],
+  delivery_id: '645760ea1879fd0016ad3015',
+  totalPrice: 3000,
+  order_status: 'Pending',
+  payment_status: 'Pending',
+}
+
+export const updatedOrderStatus = {
+  order_id: 'OID003',
+  user_id: '64562c282fb20f06a113bde4',
+  products: [
+    {
+      product_id: '643e85d9f231b52fe5162523',
+      quantity: 3,
+    },
+  ],
+  delivery_id: '645760ea1879fd0016ad3015',
+  totalPrice: 3000,
+  order_status: 'Confirmed',
+  payment_status: 'Pending',
+}
+
+export const updatedPaymentStatus = {
+  order_id: 'OID003',
+  user_id: '64562c282fb20f06a113bde4',
+  products: [
+    {
+      product_id: '643e85d9f231b52fe5162523',
+      quantity: 3,
+    },
+  ],
+  delivery_id: '645760ea1879fd0016ad3015',
+  totalPrice: 3000,
+  order_status: 'Confirmed',
+  payment_status: 'Paid',
+}
+
+export const invalidOrder = {
+  order_id: 'OID004',
+  user_id: '64562c282fb20f06a113bde4',
+  products: [
+    {
+      product_id: '643e85d9f231b52fe5162523',
+      quantity: 3,
+    },
+  ],
+  delivery_id: '645760ea1879fd0016ad3015',
+  totalPrice: 3000,
+  order_sts: 'Pending',
+  payment_sts: 'Pending',
+}
